ecee187a0dd89526c01630f559523dccc5a09edc
Mohd. Rizwan Shaikh <mohdrizwan.shaikh2112@gmail.com>
2024-05-19 15:09:57 +0530

    Implemented UI for consent management system.

---
+++ doctor/src/components/Login/NavBar/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar"
+import './styles.css'
+function NavbarMain() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <h1>Swastha Suraksha</h1>
+      </div>
+    </>
+  )
+}
+
+export default NavbarMain
